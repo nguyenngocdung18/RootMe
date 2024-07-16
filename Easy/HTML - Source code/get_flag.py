1335fc49a2b374b0c302944125f37713fe7dbc10
nguyenngocdung18 <134156226+nguyenngocdung18@users.noreply.github.com>
2024-07-16 10:14:52 +0700

    Update get_flag.py

--- Easy/HTML - Source code/get_flag.py
+++ Easy/HTML - Source code/get_flag.py
@@ -5,17 +5,14 @@ url = "http://challenge01.root-me.org/web-serveur/ch1/"
 # Thực hiện yêu cầu GET tới URL
 response = requests.get(url)
 
-# Kiểm tra xem yêu cầu có thành công không
-if response.status_code == 200:
-    # Lấy nội dung trang
-    content = response.text
+# Lấy nội dung trang
+content = response.text
     
-    # Tìm flag
-    start_index = content.find("password : ") + len("password : ")
-    end_index = content.find("\n", start_index)
-    flag = content[start_index:end_index].strip()
+# Tìm flag
+start = content.find("password : ") + len("password : ")
+end = content.find("\n", start)
+flag = content[start:end].strip()
     
-    # In flag
-    print(f"The flag is: {flag}")
-else:
-    print(f"Failed to retrieve the page. Status code: {response.status_code}")
+# In flag
+print(f"The flag is: {flag}")
+
